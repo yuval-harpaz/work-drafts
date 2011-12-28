4af36d032b24f845b2e93d398501fa643f487d19
yuval <yuval@bar-ilan>
2011-12-28 08:32:32 +0200

    adding betweenGroups ttest

---
+++ matlab/betweenGroups.m
@@ -0,0 +1,21 @@
+function betweenGroups(subs1,subs2,tlrcFile,prefix)
+% SET2-SET1 !
+cd ~/Desktop/talResults
+SET1='';
+SET2='';
+if ~exist('prefix','var')
+    prefix='';
+else
+    prefix=[' -prefix ',prefix];
+end
+for sub1i=1:length(subs1)
+    sub1=subs1{sub1i};
+    SET1=[SET1,sub1,'/',tlrcFile,' '];
+end
+for sub2i=1:length(subs2)
+    sub2=subs2{sub2i};
+    SET2=[SET2,sub2,'/',tlrcFile,' '];
+end
+eval(['!~/abin/3dttest',prefix,' -set1 ',SET1,'-set2 ',SET2]);
+%display(['!~/abin/3dttest -paired -set1 ',SET1,'-set2 ',SET2]);
+end
