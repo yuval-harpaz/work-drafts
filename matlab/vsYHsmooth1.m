a6899d58c0708b52cdce31f83ada241306e4509e
yuval <yuval@bar-ilan>
2012-08-07 22:04:42 +0300

    RMS for wts

--- matlab/vsYHsmooth1.m
+++ matlab/vsYHsmooth1.m
@@ -3,12 +3,12 @@ load VG
 timeStep=20;
 [vs,timeline,allInd]=VS_slice(VG,'SAM/VGerf,1-35Hz,VerbAa.wts',timeStep,[0 0.7]);
 [vs,allInd]=inScalpVS(vs,allInd);
-TR=num2str(1000*timeStep/1017.25)
+TR=num2str(timeStep/1017.25)
 %% raw movie
 
 vsSlice2afni(allInd,vs,'raw',true); % abs
-!~/abin/3dTcat -prefix catRaw raw*
-!~/abin/3drefit -TR 19.66 catRaw+orig
+!~/abin/3dTcat -prefix catRaw raw*.HEAD
+!~/abin/3drefit -TR 0.01966 catRaw+orig
 %% raw no absolute values
 cd plusMinus
 !cp ../ortho+orig.* ./
@@ -20,7 +20,7 @@ vsZ=zScoreVS(vs); %standardize channels to avoid bias to medial vs.
 cd Z
 !cp ../ortho+orig.* ./
 vsSlice2afni(allInd,vsZ,'Z',true);
-!~/abin/3dTcat -prefix catZ Z*
+!~/abin/3dTcat -prefix catZ Z*.HEAD
 !~/abin/3drefit -TR 0.01966 catZ+orig
 % -Torg 0 by default
 %% Pseudo Z
@@ -36,7 +36,7 @@ vsPZ=(vs-meanmat)./sdmat;
 cd pseudoZ
 !cp ../ortho+orig.* ./
 vsSlice2afni(allInd,vsPZ,'PZ');
-!~/abin/3dTcat -prefix catPZ PZ*
+!~/abin/3dTcat -prefix catPZ PZ*.HEAD
 !~/abin/3drefit -TR 0.01966 catPZ+orig
 
 mkdir press
@@ -62,8 +62,8 @@ for i=1:9;eval(['!cp vgPZ',num2str(i),'+orig.BRIK vgPZ0',num2str(i),'+orig.BRIK'
 TR=num2str(20*1000/1017.25);
 eval(['!~/abin/3drefit -TR ',TR,' CATvgPZ+orig'])
 !cp CATvgPZ* tests/
-kurmap20=kurtosis(vs',0)';
-
+% kurmap20=kurtosis(vs',0)';
+% 
 
 %Stephen's g2 for large samples, without reducing 3 to avoid neg values.
 kur=g2(vs);
@@ -79,3 +79,71 @@ vsSlice2afni(allInd,kur,'kur');
 vsSlice2afni(allInd,kurmap20,'kur20c');
 !~/abin/3dcalc -a kur20c1+orig -b CatVGtr+orig -expr 'ispositive(a-4)*b' -prefix CatVGtrKur
 !~/abin/3dcalc -a clust_mask+orig -b CATvgPZ+orig -expr 'b*ispositive(a)' -prefix CatKurSNR
+
+%% weights based normalization: RMS + Max
+cd /home/yuval/Data/tel_hashomer/yuval
+load VG
+timeStep=20;
+[vs,timeline,allInd]=VS_slice(VG,'SAM/VGerf,1-35Hz,VerbAa.wts',timeStep,[0 0.7]);
+[vs,allInd]=inScalpVS(vs,allInd);
+TR=num2str(timeStep/1017.25)
+%
+load('/home/yuval/Data/tel_hashomer/yuval/SAM/VGerf,1-35Hz,VerbAa.mat')
+boxSize=[-12 12 -9 9 -2 15];
+step=0.5;
+AP=-12:0.5:12;LR=-9:0.5:9;IS=-2:0.5:15;
+rmsi=0;
+for voxi=AP
+    for voxj=LR
+        for voxk=IS
+            [ind,~]=voxIndex([voxi,voxj,voxk],boxSize,step);
+            wts=ActWgts(ind,:);
+            rmsi=rmsi+1;
+            rmsWts(rmsi)=sqrt(mean(wts.^2));
+            maxWts(rmsi)=max(abs(wts));
+        end
+    end
+end
+figure;plot(rmsWts)
+RMS=rmsWts(find(rmsWts>0));
+RMS=RMS';
+RMS=vec2mat(RMS,36);
+vsRMS=vs./RMS;
+vsSlice2afni(allInd,vsRMS,'rms'); % abs
+!~/abin/3dTcat -prefix catRms rms*.HEAD
+!~/abin/3drefit -TR 0.01966 catRms+orig
+% rescale, values too small for AFNI graphs
+!~/abin/3dcalc -a catRms+orig -expr 'a*1e+13' -prefix catRmsSc
+
+%figure;plot(LR,maxWts)
+
+% Max=maxWts(find(rmsWts>0));
+% Max=Max';
+% Max=vec2mat(Max,36);
+% vsMax=vs./Max;
+% vsSlice2afni(allInd,vsMax,'max'); % abs
+% !~/abin/3dTcat -prefix catMax max*.HEAD
+% eval(['!~/abin/3drefit -TR ',TR,' catMax+orig'])
+% 
+%% kurtosis mask
+kur=g2(vs);
+vsSlice2afni(allInd,kur,'KurMask');
+cd RMS
+!~/abin/3dcalc -a catRmsSc+orig -b ../KurMask001+orig -expr 'a*ispositive(b-3)' -prefix catRmsKmsk
+!~/abin/3dcalc -a catRmsSc+orig -b ../KurMask001+orig -expr 'a*ispositive(a-3)*ispositive(b-3)+3*ispositive(a)' -prefix catRms_Kur3Amp3
+
+%% high temp res
+cd /home/yuval/Data/tel_hashomer/yuval/RMShr
+load ../VG
+load rmsWts
+timeStep=2;
+[vs,timeline,allInd]=VS_slice(VG,'../SAM/VGerf,1-35Hz,VerbAa.wts',timeStep,[0 0.5]);
+[vs,allInd]=inScalpVS(vs,allInd);
+TR=num2str(timeStep/1017.25)
+RMS=rmsWts(find(rmsWts>0));
+RMS=RMS';
+RMS=vec2mat(RMS,size(vs,2));
+vsRMS=vs./RMS;
+vsSlice2afni(allInd,vsRMS,'rms'); % abs
+!~/abin/3dTcat -prefix catRms rms*.HEAD
+eval(['!~/abin/3drefit -TR ',TR,' catRms+orig']);
