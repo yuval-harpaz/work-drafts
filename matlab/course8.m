4271bd1b0007cfd0d8c40c4eea0bd8ec741e2a42
yuval <yuval@bar-ilan>
2013-03-21 19:15:32 +0200

    course8 changes

--- matlab/course8.m
+++ matlab/course8.m
@@ -1,4 +1,10 @@
-cd oddball
+%% use SAMwts with fieldtrip
+% make a text file with grid points
+% here the grid points are 10mm spaced on template. the spacing is modified
+% for each subject
+
+
+
 
 % first we want a headmodel
 % put debug points here: in headmodel_BIU at lines 119 (D.inv{1}...) and
@@ -10,63 +16,31 @@ load ~/ft_BIU/matlab/files/sMRI.mat
 mri_realign=sMRI;
 mri_realign.transform=inv(M1)*sMRI.transform;
 grid2t(grid);
-
-load trl
-trigTime=(trl(:,1)-trl(:,3))./1017.25;
-Trig2mark(...
-    'All',trigTime',...
-    'Standard',trigTime(find(trl(:,4)==128),1)',...
-    'Oddball',trigTime(find(trl(:,4)==64),1)',...
-    'Novel',trigTime(find(trl(:,4)==32),1)');
-
-toi=[0.043657 0.075163];
-
-cd ..
-createPARAM('allTrials','ERF','All',toi,'All',[(toi(1)-toi(2)) 0],[1 40],[-0.1 0.5],[],[],'MultiSphere');
-!SAMcov -r oddball -d c,rfhp0.1Hz -m allTrials -v
-!SAMwts -r oddball -d c,rfhp0.1Hz -m allTrials -c Alla -v
-!SAMerf -r oddball -d c,rfhp0.1Hz -m allTrials -v -z 3
-% view results
-cd oddball
-!cp SAM/*.svl ./
-!~/abin/afni -dset warped+orig &
-
 % make new weights based on individually fit grid (pnt.txt)
+% this works only after SAMcov (course7)
+cd ..
 !cp pnt.txt SAM/pnt.txt
 !SAMwts -r oddball -d c,rfhp0.1Hz -m allTrials -c Alla -t pnt.txt -v
+
 
 cd oddball/SAM;
 wtsNoSuf='pnt.txt';
 [SAMHeader, ActIndex, ActWgts]=readWeights([wtsNoSuf,'.wts']);
 save([wtsNoSuf,'.mat'],'SAMHeader', 'ActIndex', 'ActWgts');
 filter=wts2filter(ActWgts,grid.inside,size(grid.outside,1));
+cd ../
+load data
+dataSt=data;
+trli=find(data.trialinfo==128);
+dataSt.trial=data.trial(1,trli);
+dataSt.time=data.time(1,trli);
+dataStAvg=ft_timelockanalysis([],dataSt);
+timewin=[0.043657 0.075163];
 
-
-
-
-%% messy below, don't look
- % this was calculated in course6 (auditory data)
-
-cd oddball;
-wtsNoSuf='SAM/allTrials,1-40Hz,Alla';
-[SAMHeader, ActIndex, ActWgts]=readWeights([wtsNoSuf,'.wts']);
-save([wtsNoSuf,'.mat'],'SAMHeader', 'ActIndex', 'ActWgts'); %sa
-vox=[1.5,-5.5,5.5];
-% lets visualize the weights used for source loc to this voxel
-plotWeights(wtsNoSuf,vox)
-
-% now we want a virtual sensor created for this voxel to be created for 2
-% conditions
-[voxi,allInd]=voxIndex(vox,100.*[...
-    SAMHeader.XStart SAMHeader.XEnd ...
-    SAMHeader.YStart SAMHeader.YEnd ...
-    SAMHeader.ZStart SAMHeader.ZEnd],...
-    100.*SAMHeader.StepSize,1);
-wts=ActWgts(voxi,:);
-vsSt=ActWgts(voxi,:)*standard.avg;
-
-[vol,grid,mesh,M1]=headmodel_BIU([],[],[],[],'localspheres');
-load ~/ft_BIU/matlab/files/sMRI.mat
-mri_realign=sMRI;
-mri_realign.transform=inv(M1)*sMRI.transform;
-
+% use weights from SAM (Dr. Robinson's) to make images with fieldtrip
+sourceTest=OBbeamform(dataSt,timewin,'SAM',mri_realign,filter)
+% make similar weights from raw data with fieldtrip version of SAM.
+% multiply by averaged data, like SAMerf
+sourceTest=OBbeamform(dataSt,timewin,'sam',mri_realign)
+% compute weights on averaged data. Dr. Robinson won't approve.
+sourceTest=OBbeamform(dataStAvg,timewin,'sam',mri_realign)
