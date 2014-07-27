7e984c668f723305108eef305aca78642051deb8
yuval <yuval@bar-ilan>
2014-07-27 16:26:38 +0300

    g2 for a vector

--- matlab/g2.m
+++ matlab/g2.m
@@ -1,10 +1,17 @@
 function G2=g2(x)
-% x is a data matrix, raws for channels, columns for time samples.
-xbl=x-vec2mat(mean(x,2),size(x,2));
+% x is a data matrix (or a vector), raws for channels, columns for time samples.
+if size(x,2)==1
+    x=x';
+end
+if size(x,1)==1
+    xbl=x-mean(x,2);
+else
+    xbl=x-vec2mat(mean(x,2),size(x,2));
+end
 x2=xbl.^2;
 x4=xbl.^4;
 sx2=sum(x2')';
 sx4=sum(x4')';
-VAR=sx2./(size(x,2)-1); 
+VAR=sx2./(size(x,2)-1);
 G2=sx4./(VAR.*VAR.*size(x,2)); %here the -3 was deleted to avoid negative g2.
 end
