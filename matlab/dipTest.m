b38ce8e0c39473ac01afb02578a445a7ed4ae5f7
yuval-harpaz <yuvharpaz@gmail.com>
2015-06-22 14:16:55 +0300

    gitUpdate

--- matlab/dipTest.m
+++ matlab/dipTest.m
@@ -6,8 +6,8 @@ Grid=grid;
 clear grid
 load ../avgFilt avg1_handR
 data=avg1_handR;clear avg*
-
-
+hs=ft_read_headshape('hs_file')
+hs=hs.pnt*1000;
 
 cfg=[];
 cfg.latency=[data.time(138) data.time(138)];
@@ -15,27 +15,27 @@ cfg.nonlinear='no';
 cfg.grid=Grid;
 cfg.symmetry='y';
 cfg.method='pinv';
-dip2pinv=dipolefitBIU(cfg,data);
+[dip2pinv,Rpinv]=dipolefitBIU(cfg,data);
 
 corr(dip2pinv.Vmodel,dip2pinv.Vdata).^2
 
-cfg.method='\';
-dip2ld=dipolefitBIU(cfg,data);
-corr(dip2ld.Vmodel,dip2ld.Vdata).^2
-
-
 cfg.method='*';
-dip2mu=dipolefitBIU(cfg,data);
+[dip2mu,Rmu]=dipolefitBIU(cfg,data);
 corr(dip2mu.Vmodel,dip2mu.Vdata).^2
 
-
-
-
-
-
-
+figure;
 scatter3pnt(hs,1,'k');hold on
-scatter3pnt(cfg.grid.pos(cfg.grid.inside,:),25,R(cfg.grid.inside))
+scatter3pnt(cfg.grid.pos(cfg.grid.inside,:),25,Rmu(cfg.grid.inside))
+view(160,0)
+figure;
+scatter3pnt(hs,1,'k');hold on
+scatter3pnt(cfg.grid.pos(cfg.grid.inside,:),25,Rpinv(cfg.grid.inside))
+view(160,0)
+
+cfg.method='fieldTrip';
+[dip2ft]=dipolefitBIU(cfg,data);
+corr(dip2ft.Vmodel,dip2ft.Vdata).^2
+
 
 
 
