859994b44e64ba62e5f6a4ee1989238ce161c437
yuval-harpaz <yuvharpaz@gmail.com>
2015-08-24 17:00:57 +0300

    xls files for Hila & Rotem

--- matlab/charTables.m
+++ matlab/charTables.m
@@ -1,25 +1,60 @@
 %% Correlative channels between subjects
+cd('/media/yuval/My Passport/Hila&Rotem')
+%load dataA dataAord
+%%
+trigVal=[202 204 220 230 240 250;202 204 240 230 220 250];
+conds={'closed','open','charisma','room','dull','silent'};
+load Sub40
 
-load dataA dataAord
+lowFreq=1:7;
+alphaFreq=8:12;
+betaFreq=13:24;
+gammaFreq=25:40;
+bandFreq=alphaFreq;
+Four='Fr.mat';
+dataBand=zeros(40,6,5,302,248);
+for freq=1:length(bandFreq)
+    for subi=1:length(Sub)
+        cd(Sub{subi,1})
+        load(Four)
+        cond=TRL(:,4);
+        clear TRL
+        if subi==1
+            Good=zeros(40,1674);
+            Good(subi,good)=1;
+            Cond=cond';
+        else
+            Cond(subi,:)=cond;
+            Good(subi,good)=1;
+        end
+        for chan=1:248
+            datai(:,chan)=Fr.powspctrm(:,chan,freq).*Good(subi,:)';
+        end
+        for condi=1:6
+            i0=find(Cond(subi,:)==trigVal(Sub{subi,2},condi),1);
+            i1=find(Cond(subi,:)==trigVal(Sub{subi,2},condi),1,'last');
+            dataBand(subi,condi,freq,1:length(i0:i1),:)=datai(i0:i1,:);
+        end
+        clear datai
+        cd ../
+        disp([Sub{subi,1},' complete']);
+    end
+    disp(['Frequency ',num2str(bandFreq(freq)),' processed']);
+end
+dataBand(dataBand==0)=NaN;
+%%
+
 condlength=[240 240 302 302 282 302];
 conds={'closed','open','charisma','room','dull','silent'};
 
-% R=[];
-% R=corr(squeeze(dataAord(:,3,1,:,1))','rows','pairwise');
-% r=[];
-% for subi=1:size(R,1)
-%     ii=1:size(R,1);
-%     ii(subi)=[];
-%     r(subi,:)=squeeze(mean(R(ii,subi,:)));
-% end
 
-%Make plot based on correlations between subjects in most powerful channels
-%and frequencies
+
+
 for subi=1:40
     for condi=1:6
-        for freq=1:size(dataAord,3)
+        for freq=1:size(dataBand,3)
             for time=1:302
-                [maxval,maxi]=max(dataAord(subi,condi,freq,time,:));
+                [maxval,maxi]=max(dataBand(subi,condi,freq,time,:));
                 datamaxchan(subi,condi,freq,time)=maxval;
             end
         end
@@ -42,72 +77,82 @@ for condi=1:6
     for coli=1:40
         m=nanmean(data(1:condlength(condi),coli));
         data(isnan(data(:,coli)),coli)=m;
-        data=data-m;
+        %data=data-m;
     end
     data=data(1:condlength(condi),:);
-    RR=corr(data);
-    [V,D]=eig(RR);
-    PC1=V(:,end)'*data';
-    dataavg=mean(data')';
-    orient=corr(dataavg,PC1');
-    if orient<0
-        PC1=-PC1;
+    means(1:40,condi)=mean(data);
+end
+means(37,:)=[];
+labels={'closed','open','char','room','dull','silent'};
+%[~,p]=ttest(means(:,3),means(:,5))
+%plot(mean(means'))
+bars=mean(means,1);
+err=std(means)./sqrt(size(means,1));
+figure, hold all;
+bar(bars,'w');
+errorbar(bars,err,'k','linestyle','none')
+bar(bars,'w');
+bar(3,bars(3),'b');
+bar(5,bars(5),'r');
+title('mean Alpha ')
+set(gca,'XTick',1:6);
+set(gca,'XTickLabel',labels);
+
+if ~exist('org.apache.poi.ss.usermodel.WorkbookFactory', 'class')
+    cd ~/Documents/MATLAB/20130227_xlwrite/20130227_xlwrite
+    javaaddpath('poi_library/poi-3.8-20120326.jar');
+    javaaddpath('poi_library/poi-ooxml-3.8-20120326.jar');
+    javaaddpath('poi_library/poi-ooxml-schemas-3.8-20120326.jar');
+    javaaddpath('poi_library/xmlbeans-2.3.0.jar');
+    javaaddpath('poi_library/dom4j-1.6.1.jar');
+end
+cd('/media/yuval/My Passport/Hila&Rotem')
+Sub(37,:)=[];
+table={};
+table{1,1}='subjects\condition';
+for coli=1:6
+    table{1,coli+1}=labels{coli};
+    for rowi=1:39
+        if coli==1
+            table{rowi+1,1}=Sub{rowi,1};
+        end
+        table{rowi+1,coli+1}=means(rowi,coli)*10e24;
     end
-    figure;plot(PC1)
-    hold on
-    title(['Main Comp Alpha, cond: ',conds(condi)]);
-    
-    %Create smoothed, time-adjusted plot
-    for tscale=1:floor((condlength(condi)-9)/5+1)
-        tstart=1+5*(tscale-1);
-        movAvgmax(tscale)=sum(PC1(tstart:(tstart+9)))/10;
+end
+xlwrite('means',table);
+%% Correlations
+load audioChar
+load audioDull
+data=squeeze(datamax(:,3,:))';
+data(:,37)=[];
+thr=nanmedian(data(:))*40;
+data(data>thr)=nan;
+data=data(1:condlength(condi),:);
+timeCourseChar=nanmean(data,2)*10e24;
+timeCourseChar(:,2)=audioChar;
+
+data=squeeze(datamax(:,5,:))';
+data(:,37)=[];
+thr=nanmedian(data(:))*40;
+data(data>thr)=nan;
+data=data(1:condlength(5),:);
+timeCourseDull=nanmean(data,2)*10e24;
+timeCourseDull(:,2)=audioDull;
+[r,p]=corr(timeCourseChar);
+[r,p]=corr(timeCourseDull);
+table={};
+%table{1,1}='subjects\condition';
+table{1,1}='brainChar';
+table{1,2}='audioChar';
+table{1,3}='brainDull';
+table{1,4}='audioDull';
+for rowi=1:302
+    table{rowi+1,1}=timeCourseChar(rowi,1);
+    table{rowi+1,2}=timeCourseChar(rowi,2);
+    if rowi<283
+        table{rowi+1,3}=timeCourseDull(rowi,1);
+        table{rowi+1,4}=timeCourseDull(rowi,2);
     end
-    timescale=[1:length(movAvgmax)].*2.5;
-    figure;
-    plot(timescale,movAvgmax)
-    title(['smoothed main comp alpha, cond: ',conds(condi)]);
-    
-    clear data RR V PC1 movAvgmax
 end
 
-
-
-%% Correlative channels within brain regions
-
-posteriorR = {'A51', 'A52', 'A77', 'A78', 'A79', 'A107', 'A108', 'A109', 'A110', 'A139', 'A140', 'A141', 'A142', 'A167', 'A168', 'A169', 'A170', 'A188', 'A189', 'A190', 'A191', 'A206', 'A207', 'A208', 'A225'};
-anteriorR = {'A54', 'A55', 'A81', 'A82', 'A83', 'A112', 'A113', 'A114', 'A115', 'A144', 'A145', 'A146', 'A171', 'A172', 'A173', 'A174', 'A193', 'A209', 'A210', 'A211', 'A227', 'A244', 'A245', 'A246', 'A247'};
-load LRpairs
-[~,Li]=ismember(anteriorR,LRpairs(:,2));
-anteriorL=LRpairs(Li,1);
-[~,Li]=ismember(posteriorR,LRpairs(:,2));
-posteriorL=LRpairs(Li,1);
-
-cd Char_1
-load Fr
-cd ../
-[~,chi]=ismember(anteriorR,Fr.label);
-antR=dataAord(:,:,:,:,chi);
-[~,chi]=ismember(posteriorR,Fr.label);
-postR=dataAord(:,:,:,:,chi);
-[~,chi]=ismember(anteriorL,Fr.label);
-antL=dataAord(:,:,:,:,chi);
-[~,chi]=ismember(posteriorL,Fr.label);
-postL=dataAord(:,:,:,:,chi);
-clear Fr
-
-for section=1:4
-    switch section
-        case 1
-            sect=antL;
-            secttitle='antL';
-        case 2
-            sect=antR;
-            secttitle='antR';
-        case 3
-            sect=postL;
-            secttitle='postL';
-        case 4
-            sect=postR;
-            secttitle='postR';
-    end
-end 
+xlwrite('timecourse',table);
