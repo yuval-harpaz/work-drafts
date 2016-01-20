3276768b08ee03fad0112488982810e6f63c05ba
yuval-harpaz <yuvharpaz@gmail.com>
2016-01-20 14:14:27 +0200

    gitUpdate

--- matlab/b397.m
+++ matlab/b397.m
@@ -1,119 +1,68 @@
-cd /media/yuval/win_disk/Data/epilepsy2/b397/2
+cd /media/yuval/win_disk/Data/epilepsy2/b397
 cfg=[];
-cfg.dataset='c,rfhp1.0Hz';
-cfg.trl=[1,1017.25*4,0];
+cfg.dataset='2/c,rfhp1.0Hz';
+cfg.trl=[1,1017.25*8,0];
 cfg.channel='MEG';
 cfg.lpfilter='yes';
 cfg.lpfreq=70;
+cfg.demean='yes';
 MEG=ft_preprocessing(cfg);
+MEG=ft_timelockanalysis([],MEG);
+cfg=[];
+cfg.layout='4D248.lay';
+cfg.interactive='yes';
+cfg.xlim=[3.93 3.93];
+cfg.zlim=[-2.5e-12 2.5e-12];
+figure;ft_topoplotER(cfg,MEG);
+
+
+
 
 for ei=1:32
     labels{ei,1}=['E',num2str(ei)];
 end
 
 cfg=[];
-cfg.dataset='c,rfhp1.0Hz';
-cfg.trl=[1,1017.25*4,0];
+cfg.dataset='2/c,rfhp1.0Hz';
+cfg.trl=[1,1017.25*8,0];
 cfg.channel = labels;
 % cfg.lpfilter='yes';
 % cfg.lpfreq=70;
 % cfg.demean='yes';
 EEG=ft_preprocessing(cfg);
 
-load rsEEG
-rsEEG=rsEEG(:,1:1017.25*4);
+load 2/rsEEG
+rsEEG=rsEEG(:,1:1017.25*8);
 EEG.trial{1}(:,:)=rsEEG;
 EEG.trial{1}(13,:)=[];
 EEG.trial{1}(19,:)=[];
 %EEG.label = {'Fp1'; 'Fpz'; 'Fp2'; 'F7'; 'F3'; 'Fz'; 'F4'; 'F8'; 'FC5'; 'FC1'; 'FC2'; 'FC6'; 'M1'; 'T7'; 'C3'; 'Cz'; 'C4'; 'T8'; 'M2'; 'CP5'; 'CP1'; 'CP2'; 'CP6'; 'P7'; 'P3'; 'Pz'; 'P4'; 'P8'; 'POz'; 'O1'; 'Oz'; 'O2'};
 EEG.label = {'Fp1'; 'Fpz'; 'Fp2'; 'F7'; 'F3'; 'Fz'; 'F4'; 'F8'; 'FC5'; 'FC1'; 'FC2'; 'FC6'; 'T7'; 'C3'; 'Cz'; 'C4'; 'T8'; 'CP5'; 'CP1'; 'CP2'; 'CP6'; 'P7'; 'P3'; 'Pz'; 'P4'; 'P8'; 'POz'; 'O1'; 'Oz'; 'O2'};
 EEG=ft_timelockanalysis([],EEG)
-cd ../
+
 cfg=[];
 cfg.layout='WG32.lay';
 cfg.interactive='yes';
-cfg.xlim=[2.95 2.95];
-cfg.zlim=[-0.1 0.1];
-ft_topoplotER(cfg,EEG);
+cfg.xlim=[3.926 3.926];
+cfg.zlim=[-0.15 0.15];
+figure;ft_topoplotER(cfg,EEG);
 
 %%
+[g2sum,g2max]=g2loop(EEG.avg,1017,0);
 
+topoplot30(g2sum);
+topoplot30(g2max);
+[g2sum,g2max]=g2loop(rsEEG,10172,0);
+topoplot32(g2sum);
+topoplot32(g2max);
 
+%% talk2
+cfg=[];
+cfg.layout='4D248.lay';
+cfg.interactive='yes';
+cfg.xlim=[3.09 3.09];
+cfg.zlim=[-2e-12 2e-12];
+cfg.comment='no';
+figure;ft_topoplotER(cfg,MEG);
 
-
-% 
-% figure;plot(MEG.trial{1}(14,:))
-% spike1=3402;
-% t1=MEG.time{1}(spike1);
-% 
-% cfg=[];
-% cfg.layout='4D248.lay';
-% cfg.interactive='yes';
-% cfg.xlim=[t1 t1];
-% cfg.zlim=[-3e-12 3e-12];
-% figure;ft_topoplotER(cfg,ft_timelockanalysis([],MEG));
-% 
-% M=MEG.trial{1}(:,spike1);
-% M(chi)=0;
-% cfg=[];
-% cfg.zlim=[-3e-12 3e-12];
-% cfg.interpolate='linear';
-% cfg.style = 'straight';
-% figure;topoplot248(M,cfg)
-% 
-% load rsEEG
-% E=rsEEG(:,spike1)
-% 
-% cd ../
-% topoplot32(E)
-% 
-% 
-% 
-% for ei=1:32
-%     labels{ei,1}=['E',num2str(ei)];
-% end
-% cfg=[];
-% cfg.dataset='c,rfhp1.0Hz';
-% cfg.trl=[1,1017.25*4,0];
-% cfg.channel = labels;
-% cfg.lpfilter='yes';
-% cfg.lpfreq=70;
-% cfg.demean='yes';
-% EEG=ft_preprocessing(cfg);
-% EEG.trial{1}(13,:)=[];
-% EEG.trial{1}(19,:)=[];
-% %EEG.label = {'Fp1'; 'Fpz'; 'Fp2'; 'F7'; 'F3'; 'Fz'; 'F4'; 'F8'; 'FC5'; 'FC1'; 'FC2'; 'FC6'; 'M1'; 'T7'; 'C3'; 'Cz'; 'C4'; 'T8'; 'M2'; 'CP5'; 'CP1'; 'CP2'; 'CP6'; 'P7'; 'P3'; 'Pz'; 'P4'; 'P8'; 'POz'; 'O1'; 'Oz'; 'O2'};
-% EEG.label = {'Fp1'; 'Fpz'; 'Fp2'; 'F7'; 'F3'; 'Fz'; 'F4'; 'F8'; 'FC5'; 'FC1'; 'FC2'; 'FC6'; 'T7'; 'C3'; 'Cz'; 'C4'; 'T8'; 'CP5'; 'CP1'; 'CP2'; 'CP6'; 'P7'; 'P3'; 'Pz'; 'P4'; 'P8'; 'POz'; 'O1'; 'Oz'; 'O2'};
-% cd ../
-% spike1=3402;
-% t1=EEG.time{1}(spike1);
-% cfg=[];
-% cfg.layout='WG32.lay';
-% cfg.interactive='yes';
-% cfg.xlim=[3.06 3.06];
-% cfg.zlim=[-0.0005 0.0005];
-% ft_topoplotER(cfg,ft_timelockanalysis([],EEG));
-% 
-% 
-% 
-% %% rescale EEG
-% cd /media/yuval/win_disk/Data/epilepsy2/b397/2
-% for ei=1:32
-%     labels{ei,1}=['E',num2str(ei)];
-% end
-% cfg=[];
-% cfg.dataset='c,rfhp1.0Hz';
-% cfg.trl=[1,1017.25*4,0];
-% cfg.channel = labels;
-% % cfg.lpfilter='yes';
-% % cfg.lpfreq=70;
-% % cfg.demean='yes';
-% EEG=ft_preprocessing(cfg);
-% EEG.trial
-% load rsEEG
-% rsEEG=rsEEG(:,1:1017.25*4);
-% %r=corr(rsEEG',EEG.trial{1}','rows','pairwise');
-% 
-% 
-% 
-% plot(EEG.trial{1}(1,:));hold on;plot(rsEEG(1,1:length(EEG.time{1}))./70,'r')
+%% 
